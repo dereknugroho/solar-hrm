cf88859fd8c69c8122fde1caa6f59549481d7b2a
Derek <dereknugroho@gmail.com>
2025-10-28 19:59:00 -0300

    Change to absolute import

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,7 +1,7 @@
 """Load and expose configuration settings for the project."""
 import json
 
-from utils.paths import from_root
+from src.utils.paths import from_root
 
 def load_config(filename: str = "config.json") -> dict:
     """Load a JSON configuration file from the project root."""
