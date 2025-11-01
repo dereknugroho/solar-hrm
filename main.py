3549c1f5669483a06d1d7269dbf1794b0ed58ddf
Derek <dereknugroho@gmail.com>
2025-11-01 12:09:08 -0300

    Test preprocessing functions from main

--- main.py
+++ main.py
@@ -1,5 +1,42 @@
+import os
+
+import pandas as pd
+
+from src.data_preprocessing import preprocess
+from src.utils.config import PREPROCESSING
+from src.utils.paths import from_root
+
+# Show all rows
+pd.set_option('display.max_rows', None)
+
+# Show all columns
+pd.set_option('display.max_columns', None)
+
 def main():
-    pass
+    #################
+    # Preprocessing #
+    #################
+    solar_df = preprocess(
+        use_preprocessed=os.path.exists(
+            from_root(PREPROCESSING['filepaths']['parquet_processed'])
+        )
+    )
+
+    # Show column information for solar_df
+    for col in solar_df.columns:
+        print(f'Column:\t{col} [{solar_df[col].dtype}]')
+        print(f'Number of unique values in {col}: {solar_df[col].nunique()}')
+        print(f'Number of NA values in {col}: {solar_df[col].isna().sum()}')
+        print('===========================')
+
+    # print(f'{solar_df.head(50)}')
+    # print(f'===================')
+    # print(f'{solar_df.tail(50)}')
+
+    #############################
+    # Exploratory Data Analysis #
+    #############################
+
 
 if __name__ == '__main__':
     main()
