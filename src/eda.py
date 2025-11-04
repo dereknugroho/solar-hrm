508abb8f9e8c59b07ad2b20addab2a39320ede42
Derek <dereknugroho@gmail.com>
2025-11-04 14:05:54 -0400

    Implement sequential and non-sequential time segmentation functions

--- src/eda.py
+++ src/eda.py
@@ -8,11 +8,20 @@ if __package__ is None:
         sys.path.append(str(project_root))
 
 import matplotlib.pyplot as plt
+import pandas as pd
 
-from src.utils.config import FILEPATHS
+from src.utils.config import AGG_METHODS, FILEPATHS, TIME_SEG, TIME_SEG_CODES
 from src.utils.utils import fetch_parquet
 
+# Show all rows
+pd.set_option('display.max_rows', None)
+
+# Show all columns
+pd.set_option('display.max_columns', None)
+
 """
+- Number of installed solar panels over time
+
 - WATTS over DATETIME
     * GROUP_BY (all, COMMUNITY_NAME, FORWARD_SORTATION_AREA)
     * AGGREGATION (sum, min, avg, median, max)
@@ -22,36 +31,47 @@ from src.utils.utils import fetch_parquet
     - Yearly (2016-2024)
 """
 
-def sum_watts_over_time(solar_df):
-    pass
-    # # Typical hour, day, month, year
-    # solar_df['hour'] = solar_df['DATETIME'].dt.hour
-    # solar_df['day'] = solar_df['DATETIME'].dt.day
-    # solar_df['month'] = solar_df['DATETIME'].dt.month
-    # solar_df['year'] = solar_df['DATETIME'].dt.year
-    # sum_typical_hour = solar_df.groupby('hour', as_index=False)['WATTS'].sum()
-    # sum_typical_day = solar_df.groupby('day', as_index=False)['WATTS'].sum()
-    # sum_typical_month = solar_df.groupby('month', as_index=False)['WATTS'].sum()
-    # sum_typical_year = solar_df.groupby('year', as_index=False)['WATTS'].sum()
-    # return sum_typical_hour, sum_typical_day, sum_typical_month, sum_typical_year
+def watts_over_time_sequential(solar_df: pd.DataFrame, seg: str, agg: str):
+    """Segment dataframe solar_df into sequential chunks of time."""
+    if agg not in AGG_METHODS:
+        raise ValueError(f'Invalid agg {agg} must be a valid aggregation method: {AGG_METHODS}')
+    if seg not in TIME_SEG_CODES:
+        raise ValueError(f'Invalid seg {seg} must be a valid time segmentation code: {TIME_SEG_CODES}')
 
-    # # Sequential hour, day, month, year
-    # sum_sequential_hour = solar_df.resample('H', on='DATETIME')['WATTS'].sum()
-    # sum_sequential_day = solar_df.resample('D', on='DATETIME')['WATTS'].sum()
-    # sum_sequential_week = solar_df.resample('W', on='DATETIME')['WATTS'].sum()
-    # sum_sequential_month = solar_df.resample('M', on='DATETIME')['WATTS'].sum()
-    # sum_sequential_year = solar_df.resample('Y', on='DATETIME')['WATTS'].sum()
-    # return sum_sequential_hour, sum_sequential_day, sum_sequential_month, sum_sequential_year
+    return getattr(solar_df.sort_values('DATETIME').resample(seg, on='DATETIME')['WATTS'], agg)()
+
+def watts_over_time_non_sequential(solar_df, seg: str, agg: str):
+    """Segment dataframe solar_df into non-sequential chunks of time."""
+    if agg not in AGG_METHODS:
+        raise ValueError(f'Invalid agg {agg} must be a valid aggregation method: {AGG_METHODS}')
+    if seg not in TIME_SEG:
+        raise ValueError(f'Invalid seg {seg} must be a valid time segmentation interval: {TIME_SEG}')
+
+    solar_df = solar_df.copy()
+    solar_df[seg] = getattr(solar_df['DATETIME'].dt, seg)
+
+    return getattr(solar_df.groupby(seg, as_index=False)['WATTS'], agg)()
 
 if __name__ == '__main__':
     solar_df = fetch_parquet(FILEPATHS['parquet_processed'])
 
-    print(solar_df.head())
+    # print(solar_df.head())
 
-    for col in solar_df.columns:
-        print(f'Column:\t{col} [{solar_df[col].dtype}]')
-        print(f'Number of unique values in {col}: {solar_df[col].nunique()}')
-        print(f'Number of NA values in {col}: {solar_df[col].isna().sum()}')
-        print('===========================')
+    # for col in solar_df.columns:
+    #     print(f'Column:\t{col} [{solar_df[col].dtype}]')
+    #     print(f'Number of unique values in {col}: {solar_df[col].nunique()}')
+    #     print(f'Number of NA values in {col}: {solar_df[col].isna().sum()}')
+    #     print('===========================')
 
-    sum_watts_over_time(solar_df)
+    # month_seg_seq_sum = watts_over_time_sequential(solar_df, seg = 'M', agg = 'sum')
+    # print(f'month_seg_seq_sum: {type(month_seg_seq_sum)}\n{month_seg_seq_sum}')
+
+    # month_seg_seq_mean = watts_over_time_sequential(solar_df, seg = 'M', agg = 'mean')
+    # print(f'month_seg_seq_mean: {type(month_seg_seq_mean)}\n{month_seg_seq_mean}')
+
+    # month_seg_non_seq_sum = watts_over_time_non_sequential(solar_df, seg = 'month', agg = 'sum')
+    # print(f'month_seg_non_seq_sum: {type(month_seg_non_seq_sum)}\n{month_seg_non_seq_sum}')
+
+    # month_seg_non_seq_mean = watts_over_time_non_sequential(solar_df, seg = 'month', agg = 'mean')
+    # print(f'month_seg_non_seq_mean: {type(month_seg_non_seq_mean)}\n{month_seg_non_seq_mean}')
+
