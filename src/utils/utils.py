a8812934b1920db8c37dfa3358c63178761969bb
Derek <dereknugroho@gmail.com>
2025-11-05 21:11:19 -0400

    Change ensure_dataframe wrapper to check all arguments

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -4,15 +4,29 @@ from functools import wraps
 
 from src.utils.config import from_root
 
-def ensure_dataframe(func):
-    """Decorator to check that the first argument is a pandas DataFrame."""
-    @wraps(func)
-    def wrapper(df, *args, **kwargs):
-        if not isinstance(df, pd.DataFrame):
-            raise TypeError(f"{func.__name__}: argument must be a pandas DataFrame")
-        return func(df, *args, **kwargs)
+# def ensure_dataframe(func):
+#     """Decorator to check that the first argument is a pandas DataFrame."""
+#     @wraps(func)
+#     def wrapper(df, *args, **kwargs):
+#         if not isinstance(df, pd.DataFrame):
+#             raise TypeError(f"{func.__name__}: argument must be a pandas DataFrame")
+#         return func(df, *args, **kwargs)
 
+#     return wrapper
+
+def ensure_dataframe(func):
+    """Decorator to check that all arguments are a pandas DataFrame."""
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        for arg in args:
+            if not isinstance(arg, pd.DataFrame):
+                raise TypeError(f"{func.__name__}: all positional arguments must be pandas DataFrames")
+        for kw, val in kwargs.items():
+            if not isinstance(val, pd.DataFrame):
+                raise TypeError(f"{func.__name__}: all keyword arguments must be pandas DataFrames")
+        return func(*args, **kwargs)
     return wrapper
+
 
 def fetch_parquet(filepath: str) -> pd.DataFrame:
     """Load processed parquet into master dataframe."""
