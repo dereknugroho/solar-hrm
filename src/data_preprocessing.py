c2d75f959cc5af79dd867e737c692c250e4c2d7d
Derek <dereknugroho@gmail.com>
2025-11-05 21:10:06 -0400

    Implement untested dataframe partitioning

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -16,17 +16,22 @@ from src.utils.utils import ensure_dataframe
 
 @ensure_dataframe
 def drop_unused_columns(solar_df: pd.DataFrame) -> pd.DataFrame:
-    """Drop unused columns WATT_HOUR and KILOWATT_HOUR."""
+    """Drop unused columns specified in config.json."""
     return solar_df.drop(columns=PREPROCESSING['drop_columns'])
 
 @ensure_dataframe
 def rename_columns(solar_df: pd.DataFrame) -> pd.DataFrame:
-    """Rename DATE column to DATETIME."""
+    """Rename columns specified in config.json."""
     return solar_df.rename(columns=PREPROCESSING['rename_columns'])
 
 @ensure_dataframe
-def clean_columns(solar_df: pd.DataFrame) -> pd.DataFrame:
-    """Apply corrections to COMMUNITY_NAME column."""
+def drop_null_PK_combinations(solar_df: pd.DataFrame) -> pd.DataFrame:
+    """Drop rows containing a null value in installation_id or timestamp."""
+    return solar_df.dropna(subset=['installation_id', 'timestamp'])
+
+@ensure_dataframe
+def standardize_column_text(solar_df: pd.DataFrame) -> pd.DataFrame:
+    """Perform ."""
     solar_df['community'] = solar_df['community'].replace(PREPROCESSING['community_name_corrections'])
     return solar_df
 
@@ -39,22 +44,106 @@ def convert_object_dtypes(solar_df: pd.DataFrame) -> pd.DataFrame:
         errors='coerce',
     )
 
+    solar_df['power_watts_5min_avg'] = solar_df['power_watts_5min_avg'].astype('float')
     solar_df['community'] = solar_df['community'].astype('category')
-    solar_df['fwd_sortation_area'] = solar_df['fwd_sortation_area'].astype('category')
 
     return solar_df
 
-def preprocess(use_preprocessed: bool) -> pd.DataFrame:
+@ensure_dataframe
+def partition_solar_data(solar_df: pd.DataFrame):
+    """
+    Decompose dataframe solar_df into two dataframes with a one-to-many relationship:
+
+    panel_installation -< meter_reading.
+    """
+    # # Construct dataframe representing solar panel installations
+    # installations = (
+    #     solar_df[[
+    #         'installation_id',
+    #         'num_panels',
+    #         'community',
+    #     ]]
+    #     .drop_duplicates(subset=['installation_id'])
+    #     .reset_index(drop=True)
+    # )
+
+    # Construct dataframe representing solar panel installations
+    installations = (
+        solar_df.loc[
+            solar_df.groupby('installation_id')['num_panels'].transform('max') == solar_df['num_panels']
+        ]
+        .drop_duplicates(subset=['installation_id'])
+        .reset_index(drop=True)
+    )
+
+    # Construct dataframe representing meter readings
+    readings = (
+        solar_df[[
+            'installation_id',
+            'timestamp',
+            'power_watts_5min_avg',
+            'energy_watt_hour'
+        ]]
+        .copy()
+    )
+
+    return installations, readings
+
+@ensure_dataframe
+def validate_schema(installations: pd.DataFrame, readings: pd.DataFrame) -> None:
+    """Ensure newly-partitioned dataframes are well-formed."""
+
+    # Ensure referential integrity
+    invalid_installations = (
+        readings
+        .loc[~readings['installation_id']
+        .isin(installations['installation_id'])]
+    )
+    if not invalid_installations.empty:
+        raise ValueError(f"Invalid installation_id found in readings:\n{invalid_installations['installation_id'].unique()}")
+
+    # Ensure one-to-many structure
+    duplicated_panel_installations = (
+        installations[
+            installations.duplicated(subset=['installation_id'], keep=False)
+        ]
+    )
+    if not duplicated_panel_installations.empty:
+        raise ValueError(f'Duplicate installation_id entries in installations:\n{duplicated_panel_installations}')
+
+    # Ensure unique timestamps per panel installation
+    duplicated_readings = (
+        readings[
+            readings.duplicated(subset=['installation_id', 'timestamp'], keep=False)
+        ]
+    )
+    if not duplicated_readings.empty:
+        raise ValueError(f'Duplicate readings found:\n{duplicated_readings}')
+
+    # Ensure a valid number of panels
+    if not installations['num_panels'].ge(1).all():
+        raise ValueError('num_panels must be an integer greater than or equal to 1')
+
+    # # Ensure a valid value for power_watts_5min_avg
+    # if not readings['power_watts_5min_avg'].ge(0).all():
+    #     raise ValueError('power_watts_5min_avg must be greater than or equal to 0.0')
+
+    # # Ensure a valid value for energy_watt_hour
+    # if not readings['energy_watt_hour'].ge(0).all():
+    #     raise ValueError('energy_watt_hour must be greater than or equal to 0.0')
+
+def preprocess(preprocessed_exists: bool = False) -> pd.DataFrame:
     """
     Perform the following preprocessing tasks:
     - Save a parquet of the unprocessed data
     - Drop unused columns
     - Rename columns
-    - Clean values
+    - Drop rows with null PK values
+    - Standardize column text values
     - Convert column types
-    - Save a parquet of the processed data
+    - Partition the data into two separate dataframes
     """
-    if not use_preprocessed:
+    if not preprocessed_exists:
         # Load csv into dataframe
         solar_df = pd.read_csv(
             from_root(FILEPATHS['input_csv']),
@@ -69,31 +158,50 @@ def preprocess(use_preprocessed: bool) -> pd.DataFrame:
         # Run preprocessing pipeline with parameters loaded from config.json
         solar_df = drop_unused_columns(solar_df)
         solar_df = rename_columns(solar_df)
-        solar_df = clean_columns(solar_df)
+        solar_df = drop_null_PK_combinations(solar_df)
+        solar_df = standardize_column_text(solar_df)
         solar_df = convert_object_dtypes(solar_df)
 
-        # Save preprocessed data into parquet
-        solar_df.to_parquet(
-            from_root(FILEPATHS['parquet_processed']),
+        # Partition solar_df in order to enforce entity integrity
+        installations, readings = partition_solar_data(solar_df)
+        validate_schema(installations, readings)
+
+        # Save partitioned data into parquets
+        installations.to_parquet(
+            from_root(FILEPATHS['parquet_panel_installation']),
             index=False,
         )
+        readings.to_parquet(
+            from_root(FILEPATHS['parquet_meter_reading']),
+            index=False,
+        )
+    else:
+        # Load panel_installation parquet into dataframe
+        installations = pd.read_parquet(from_root(FILEPATHS['parquet_panel_installation']))
+        readings = pd.read_parquet(from_root(FILEPATHS['parquet_meter_reading']))
 
-    # Load parquet into dataframe
-    solar_df = pd.read_parquet(
-        from_root(FILEPATHS['parquet_processed']),
-    )
+    return installations, readings
 
-    return solar_df
+def partitioned_parquets_exist() -> bool:
+    """Return True if all required parquet files exist."""
+    required_keys = ['parquet_panel_installation', 'parquet_meter_reading']
+    return all(os.path.exists(from_root(FILEPATHS[k])) for k in required_keys)
 
 if __name__ == '__main__':
-    solar_df = preprocess(
-        use_preprocessed=os.path.exists(
-            from_root(FILEPATHS['parquet_processed'])
-        )
+    installations, readings = preprocess(
+        preprocessed_exists=partitioned_parquets_exist()
     )
 
-    for col in solar_df.columns:
-        print(f'Column:\t{col} [{solar_df[col].dtype}]')
-        print(f'Number of unique values in {col}: {solar_df[col].nunique()}')
-        print(f'Number of NA values in {col}: {solar_df[col].isna().sum()}')
+    print(f'installations *****')
+    for col in installations.columns:
+        print(f'Column:\t{col} [{installations[col].dtype}]')
+        print(f'Number of unique values in {col}: {installations[col].nunique()}')
+        print(f'Number of NA values in {col}: {installations[col].isna().sum()}')
+        print('===========================')
+
+    print(f'readings *****')
+    for col in readings.columns:
+        print(f'Column:\t{col} [{readings[col].dtype}]')
+        print(f'Number of unique values in {col}: {readings[col].nunique()}')
+        print(f'Number of NA values in {col}: {readings[col].isna().sum()}')
         print('===========================')
