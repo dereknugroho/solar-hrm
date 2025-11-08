79b24f8a962e6e40412e986e1c900752ccf70537
Derek <dereknugroho@gmail.com>
2025-11-08 12:18:54 -0400

    Remove outdated comments, imports, and settings

--- src/eda.py
+++ src/eda.py
@@ -10,29 +10,7 @@ if __package__ is None:
 import matplotlib.pyplot as plt
 import pandas as pd
 
-from src.utils.config import AGG_METHODS, FILEPATHS, TIME_SEG, TIME_SEG_CODES
-from src.utils.utils import fetch_parquet
-
-# Show all rows
-pd.set_option('display.max_rows', None)
-
-# Show all columns
-pd.set_option('display.max_columns', None)
-
-"""
-- Number of installed solar panels over time
-    * Approach #1: Sum of solar panels for each SYSTEM_ID each day/year
-    * Approach #2: Average number of panels reporting per day/year
-
-- WATTS over DATETIME
-    * GROUP_BY (all, COMMUNITY_NAME, FORWARD_SORTATION_AREA)
-        [Need to implement: grouping by COMMUNITY_NAME and FORWARD_SORTATION_AREA]
-    * AGGREGATION (sum, min, avg, median, max)
-    - Hourly (24 hours)
-    - Daily (one month)
-    - Monthly (one year)
-    - Yearly (2016-2024)
-"""
+from src.utils import pd_config
 
 if __name__ == '__main__':
     pass
