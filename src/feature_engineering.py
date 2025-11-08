233aaaaf562d0526ec445bb8f6c317b4b0577e03
Derek <dereknugroho@gmail.com>
2025-11-08 12:19:45 -0400

    Save installations to parquet

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -14,11 +14,6 @@ from src.utils.config import FILEPATHS, FEATURE_ENGINEERING
 from src.utils.paths import from_root
 from src.utils.utils import ensure_dataframe
 
-# Show all rows and columns
-pd.set_option('display.expand_frame_repr', False)
-pd.set_option('display.max_rows', None)
-pd.set_option('display.max_columns', None)
-
 @ensure_dataframe
 def aggregate_installations(installations: pd.DataFrame) -> pd.DataFrame:
     """Generate statistics of panels_reporting for each installation."""
@@ -35,26 +30,32 @@ def aggregate_installations(installations: pd.DataFrame) -> pd.DataFrame:
         )
     )
 
+    installations_agg.to_parquet(
+        from_root(FILEPATHS['installations_v2']),
+        index=False,
+    )
+    print(f'Aggregated installations parquet generated and saved in data/02_feature_engineered')
+
     return installations_agg
 
 if __name__ == '__main__':
     installations = pd.read_parquet(from_root(FILEPATHS['installations_v1']))
     readings = pd.read_parquet(from_root(FILEPATHS['readings_v1']))
 
-    print(f'installations (count: {len(installations)}) *****')
-    for col in installations.columns:
-        print(f'Column:\t{col} [{installations[col].dtype}]')
-        print(f'Number of unique values in {col}: {installations[col].nunique()}')
-        print(f'Number of NA values in {col}: {installations[col].isna().sum()}')
-        print('===========================')
+    # print(f'installations (count: {len(installations)}) *****')
+    # for col in installations.columns:
+    #     print(f'Column:\t{col} [{installations[col].dtype}]')
+    #     print(f'Number of unique values in {col}: {installations[col].nunique()}')
+    #     print(f'Number of NA values in {col}: {installations[col].isna().sum()}')
+    #     print('===========================')
 
-    print(f'readings (count: {len(readings)}) *****')
-    for col in readings.columns:
-        print(f'Column:\t{col} [{readings[col].dtype}]')
-        print(f'Number of unique values in {col}: {readings[col].nunique()}')
-        print(f'Number of NA values in {col}: {readings[col].isna().sum()}')
-        print('===========================')
+    # print(f'readings (count: {len(readings)}) *****')
+    # for col in readings.columns:
+    #     print(f'Column:\t{col} [{readings[col].dtype}]')
+    #     print(f'Number of unique values in {col}: {readings[col].nunique()}')
+    #     print(f'Number of NA values in {col}: {readings[col].isna().sum()}')
+    #     print('===========================')
 
-    installations = aggregate_installations(installations)
+    # installations = aggregate_installations(installations)
 
-    print(f'installations (count: {len(installations)}):\n{installations}')
+    # print(f'installations (count: {len(installations)}):\n{installations}')
