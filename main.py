e6646c554122c5a73ce3126d0ffd1e4cbd49ab32
Derek <dereknugroho@gmail.com>
2025-11-08 12:18:20 -0400

    Test pipeline, add more print() statements

--- main.py
+++ main.py
@@ -2,41 +2,38 @@ import os
 
 import pandas as pd
 
-from src.data_preprocessing import preprocess
+from src.data_preprocessing import preprocess, preprocessed_parquets_exist
+from src.feature_engineering import aggregate_installations
+from src.utils import pd_config
 from src.utils.config import FILEPATHS
 from src.utils.paths import from_root
 
-# Show all rows
-pd.set_option('display.max_rows', None)
-
-# Show all columns
-pd.set_option('display.max_columns', None)
-
 def main():
-    #################
-    # Preprocessing #
-    #################
-    solar_df = preprocess(
-        use_preprocessed=os.path.exists(
-            from_root(FILEPATHS['parquet_processed'])
-        )
+    print(f'Initiating pipeline...')
+
+    # Data preprocessing
+    installations, readings = preprocess(
+        preprocessed_exists=preprocessed_parquets_exist()
     )
 
-    # Show column information for solar_df
-    for col in solar_df.columns:
-        print(f'Column:\t{col} [{solar_df[col].dtype}]')
-        print(f'Number of unique values in {col}: {solar_df[col].nunique()}')
-        print(f'Number of NA values in {col}: {solar_df[col].isna().sum()}')
+    # Feature engineering
+    installations_agg = aggregate_installations(installations)
+
+    print(f'***************\nreadings summary (count: {len(readings)})\n***************')
+    for col in readings.columns:
+        print(f'Column:\t{col} [{readings[col].dtype}]')
+        print(f'Number of unique values in {col}: {readings[col].nunique()}')
+        print(f'Number of NA values in {col}: {readings[col].isna().sum()}')
+        print('===========================')
+    
+    print(f'***************\ninstallations_agg summary (count: {len(installations_agg)})\n***************')
+    for col in installations_agg.columns:
+        print(f'Column:\t{col} [{installations_agg[col].dtype}]')
+        print(f'Number of unique values in {col}: {installations_agg[col].nunique()}')
+        print(f'Number of NA values in {col}: {installations_agg[col].isna().sum()}')
         print('===========================')
 
-    print(f'{solar_df.head()}')
-    print(f'===================')
-    print(f'{solar_df.tail()}')
-
-    #############################
-    # Exploratory Data Analysis #
-    #############################
-
+    print(f'***************\ninstallations_agg table (count: {len(installations_agg)})***************\n{installations_agg}')
 
 if __name__ == '__main__':
     main()
