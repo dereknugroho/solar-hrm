66ccb9bf38b1323d1c30d41f1d8b8edd6c0c65fc
Derek <dereknugroho@gmail.com>
2025-11-08 10:17:04 -0400

    Create separate module for pandas display settings

---
+++ src/utils/pd_config.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+def set_pandas_display_options():
+    """Configure pandas CLI display options."""
+
+    # Do not wrap columns onto new row
+    pd.set_option('display.expand_frame_repr', False)
+
+    # Always display all rows
+    pd.set_option('display.max_rows', None)
+
+    # Always display all columns
+    pd.set_option('display.max_columns', None)
+
+    print('✅ Pandas display options configured.')
+
+set_pandas_display_options()
