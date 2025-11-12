c1c4c66b99d50826c8b9b907b07313d0e5cb0d27
Derek <dereknugroho@gmail.com>
2025-11-12 11:24:26 -0400

    Remove green checkbox emoji from print()

--- src/core/utils/pd_config.py
+++ src/core/utils/pd_config.py
@@ -12,6 +12,6 @@ def set_pandas_display_options():
     # Always display all columns
     pd.set_option('display.max_columns', None)
 
-    print('\U00002705 Display options configured')
+    print('Display options configured')
 
 set_pandas_display_options()
