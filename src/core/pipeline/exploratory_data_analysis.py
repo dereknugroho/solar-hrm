d47b8dafd303407a951243e72d25cfc5892c4245
Derek <dereknugroho@gmail.com>
2025-11-13 12:51:27 -0400

    Improve implementation of plotting panels reporting over time, add function signature for plotting total energy production over time

--- src/core/pipeline/exploratory_data_analysis.py
+++ src/core/pipeline/exploratory_data_analysis.py
@@ -7,29 +7,22 @@ from core.utils.paths import from_root
 from core.utils.utils import ensure_dataframe
 from core.utils.validation import validate_installations_feature_engineered, validate_readings_feature_engineered
 
-@ensure_dataframe
-def plot_panels_reporting_over_time(clean_unpartitioned_parquet: pd.DataFrame):
-    """
-    Plot the daily number of panels reporting over time.
+def plot_panels_reporting_over_time(cleaned_unpartitioned: pd.DataFrame, save: bool = False) -> pd.DataFrame:
+    """Plot the number of panels reporting over time."""
+    df = cleaned_unpartitioned.copy()
 
-    X = date
-    Y = daily sum of total maximum panels reporting across all installations
-
-    Note: must use clean, unpartitioned parquet because partitioned data
-          does not contain information about change in panels reporting over time
-    """
     # Create a date column
-    clean_unpartitioned_parquet['date'] = clean_unpartitioned_parquet['timestamp'].dt.date
+    df['date'] = cleaned_unpartitioned['timestamp'].dt.date
 
-    # Maximum number of panels reporting for each installation on each day
+    # Obtain the maximum number of panels reporting for each installation on each date
     daily_max = (
-        clean_unpartitioned_parquet
+        df
         .groupby(['installation_id', 'date'])['panels_reporting']
         .max()
         .reset_index()
     )
 
-    # Maximum total number of panels reporting on each day
+    # Obtain the maximum total number of panels reporting for all installations on each date
     daily_sum = (
         daily_max
         .groupby('date')['panels_reporting']
@@ -41,34 +34,48 @@ def plot_panels_reporting_over_time(clean_unpartitioned_parquet: pd.DataFrame):
     plt.figure(figsize=(10,5))
     plt.scatter(daily_sum['date'], daily_sum['panels_reporting'], s=10)
     plt.title('Daily Maximum Total Panels Reporting Over Time')
-    plt.xlabel('Date')
+    plt.xlabel('Year')
     plt.ylabel('Daily Maximum Total Panels Reporting')
     plt.grid(True)
     plt.tight_layout()
+
+    if save:
+        plt.savefig(from_root(FILEPATHS['panels_reporting']))
+
     plt.show()
 
+    return daily_sum
+
+def plot_total_production_over_time(
+    installations_feature_engineered: pd.DataFrame,
+    readings_feature_engineered: pd.DataFrame,
+    save: bool = False
+):
+    """Plot total energy production over time."""
+    pass
+
 if __name__ == '__main__':
-    clean_unpartitioned_parquet = pd.read_parquet(from_root(FILEPATHS['clean_unpartitioned_parquet']))
-    print(f'------------------------------------------------')
-    print(f'| clean_unpartitioned_parquet (count: {len(clean_unpartitioned_parquet)}) |')
-    print(f'------------------------------------------------')
-    for col in clean_unpartitioned_parquet.columns:
-        print(f'Column: {col} [{clean_unpartitioned_parquet[col].dtype}] [num_unique: {clean_unpartitioned_parquet[col].nunique()}] [num_NA: {clean_unpartitioned_parquet[col].isna().sum()}]')
-    plot_panels_reporting_over_time(clean_unpartitioned_parquet)
+    cleaned_unpartitioned = pd.read_parquet(from_root(FILEPATHS['cleaned_unpartitioned']))
+    installations_feature_engineered = pd.read_parquet(from_root(FILEPATHS['installations_feature_engineered']))
+    readings_feature_engineered = pd.read_parquet(from_root(FILEPATHS['readings_feature_engineered']))
 
-    # installations_feature_engineered = pd.read_parquet(from_root(FILEPATHS['installations_feature_engineered']))
-    # readings_feature_engineered = pd.read_parquet(from_root(FILEPATHS['readings_feature_engineered']))
-    # validate_installations_feature_engineered(installations_feature_engineered)
-    # validate_readings_feature_engineered(readings_feature_engineered)
+    print(f'------------------------------------------------')
+    print(f'| cleaned_unpartitioned (count: {len(cleaned_unpartitioned)}) |')
+    print(f'------------------------------------------------')
+    for col in cleaned_unpartitioned.columns:
+        print(f'Column: {col} [{cleaned_unpartitioned[col].dtype}] [num_unique: {cleaned_unpartitioned[col].nunique()}] [num_NA: {cleaned_unpartitioned[col].isna().sum()}]')
     
-    # print(f'-------------------------------------------------')
-    # print(f'| installations_feature_engineered (count: {len(installations_feature_engineered)}) |')
-    # print(f'-------------------------------------------------')
-    # for col in installations_feature_engineered.columns:
-    #     print(f'Column: {col} [{installations_feature_engineered[col].dtype}] [num_unique: {installations_feature_engineered[col].nunique()}] [num_NA: {installations_feature_engineered[col].isna().sum()}]')
+    print(f'-------------------------------------------------')
+    print(f'| installations_feature_engineered (count: {len(installations_feature_engineered)}) |')
+    print(f'-------------------------------------------------')
+    for col in installations_feature_engineered.columns:
+        print(f'Column: {col} [{installations_feature_engineered[col].dtype}] [num_unique: {installations_feature_engineered[col].nunique()}] [num_NA: {installations_feature_engineered[col].isna().sum()}]')
 
-    # print(f'-------------------------------------------------')
-    # print(f'| readings_feature_engineered (count: {len(readings_feature_engineered)}) |')
-    # print(f'-------------------------------------------------')
-    # for col in readings_feature_engineered.columns:
-    #     print(f'Column: {col} [{readings_feature_engineered[col].dtype}] [num_unique: {readings_feature_engineered[col].nunique()}] [num_NA: {readings_feature_engineered[col].isna().sum()}]')
+    print(f'-------------------------------------------------')
+    print(f'| readings_feature_engineered (count: {len(readings_feature_engineered)}) |')
+    print(f'-------------------------------------------------')
+    for col in readings_feature_engineered.columns:
+        print(f'Column: {col} [{readings_feature_engineered[col].dtype}] [num_unique: {readings_feature_engineered[col].nunique()}] [num_NA: {readings_feature_engineered[col].isna().sum()}]')
+
+    plot_panels_reporting_over_time(cleaned_unpartitioned, save=True)
+    plot_total_production_over_time(installations_feature_engineered, readings_feature_engineered)
